1560ba6a23a19e8a0652a9e992e884798c6196e5
suchok94 <suchok94@gmail.com>
2025-04-21 21:08:04 +0300

    добавил проверку на то что файл есть, зачем то

--- modules/converter.py
+++ modules/converter.py
@@ -11,23 +11,32 @@ class AImageConverter(ABC):
 class ImageConverterJpgToPng(AImageConverter):
     
     def convert(source_path, destination_path):
-        file = os.path.basename(source_path)
-        file, e = os.path.splitext(file)
-        outfile = os.path.join(destination_path, file) + '.png'
-        try:
-            with Image.open(source_path) as im:
-                  im.save(outfile)
-        except OSError:
-            print("Не получилось сконвертировать", file, 'в png')
+        if Checker.file_exists(source_path):
+            file = os.path.basename(source_path)
+            file, e = os.path.splitext(file)
+            outfile = os.path.join(destination_path, file) + '.png'
+            try:
+                with Image.open(source_path) as im:
+                    im.save(outfile)
+            except OSError:
+                print("Не получилось сконвертировать", file, 'в png')
 
 class ImageConverterPngToJpg(AImageConverter):
         
     def convert(source_path, destination_path):
-        file = os.path.basename(source_path)
-        file, e = os.path.splitext(file)
-        outfile = os.path.join(destination_path, file) + '.jpg'
-        try:
-            with Image.open(source_path) as im:
-                  im.save(outfile)
-        except OSError:
-            print("Не получилось сконвертировать", file, 'в jpg')
+        if Checker.file_exists(source_path):
+            file = os.path.basename(source_path)
+            file, e = os.path.splitext(file)
+            outfile = os.path.join(destination_path, file) + '.jpg'
+            try:
+                with Image.open(source_path) as im:
+                    im.save(outfile)
+            except OSError:
+                print("Не получилось сконвертировать", file, 'в jpg')
+
+
+class Checker():
+
+    @abstractmethod
+    def file_exists(path):
+        return os.path.exists(path)
