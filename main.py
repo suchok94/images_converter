911009250646ba9a846dd1b61a76b7e6838be4fc
suchok94 <suchok94@gmail.com>
2025-04-21 20:44:35 +0300

    убрал app в отдельный модуль

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
+from modules.app import App
 from modules.converters_creator import ConverterCreatorJpgToPng, ConverterCreatorPngToJpg
-from modules.converter import ImageConverterJpgToPng, ImageConverterPngToJpg
-from abc import ABC, abstractmethod
-
-class App():
-
-    def __init__(self, converter_creator):
-        self.__converter_creator = converter_creator
-
-
-    def convert(self, source_path, destination_path):
-        self.__converter_creator.create_converter(source_path, destination_path)
-
-    # @classmethod
-    # def run(cls):
-    #     source_path = input('Введите путь до исходного файла: ')
-    #     destination_path = input('Введите путь до конечного файла: ')
-
-    #     image_type = source_path[-4:]
-
-    #     if image_type == '.png':
-    #         converterCreator = AConverterCreator(ImageConverterPngToJpg)
-        
-    #     elif image_type == '.jpg' or image_type == 'jpeg':
-    #         converterCreator = AConverterCreator()
-    #     converter = converterCreator.create_converter()
-    #     converter.convert(source_path, destination_path)
-    #     print('Всё получилось')
 
 def main():
-    app = App(ConverterCreatorJpgToPng())
-    app.convert('123.jpg', '321.png')
+
+    source_path = input('Введите путь до исходного файла: ')
+    destination_path = input('Введите путь до конечного файла: ')
+    image_type = source_path[-4:]
+
+    if image_type == '.png':
+        app = App(ConverterCreatorPngToJpg())
+        app.convert(source_path, destination_path)
+
+    elif image_type == '.jpg' or image_type == 'jpeg':
+        app = App(ConverterCreatorJpgToPng())
+        app.convert(source_path, destination_path)
+    
+    else: print("Не правильный путь")
 
 
 if __name__ == '__main__':
